2a35f47a230b09850160ef3b2e9e764a5cb45dc3
Ray Yano <yanorei@hotmail.co.jp>
2022-07-06 01:59:16 +0900

    Make work CORS implementation

--- src/backend/cors.rs
+++ src/backend/cors.rs
@@ -3,37 +3,24 @@ use std::path::PathBuf;
 
 use actix_cors::Cors;
 use actix_web::http::header::{ACCEPT, ACCEPT_LANGUAGE, AUTHORIZATION, CONTENT_TYPE};
-use addr::domain::Name;
-use addr::parse_domain_name;
-use once_cell::sync::Lazy;
 use anyhow::Result;
-use serde::{Serialize, Deserialize, Deserializer, Serializer};
+use once_cell::sync::Lazy;
+use serde::Deserialize;
 use serde_json::de::IoRead;
 
-static CORS_ALLOWED_DOMAINS: Lazy<ParsedAllowOrigin<'_>> = Lazy::new(|| load_allowed_origins().unwrap());
+static CORS_ALLOWED_DOMAINS: Lazy<ParsedAllowOrigin> =
+    Lazy::new(|| load_allowed_origins().unwrap());
 
-#[derive(Debug, Eq, PartialEq, Default)]
-struct ParsedAllowOrigin<'a>(Vec<Name<'a>>);
+type ParsedAllowOrigin = Vec<String>;
 
-impl<'de> Deserialize<'de> for ParsedAllowOrigin<'de> {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
-        Ok(Self(Vec::deserialize(deserializer)?))
-    }
-}
-
-impl Serialize for ParsedAllowOrigin<'_> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-        self.0.serialize(serializer)
-    }
-}
-fn load_allowed_origins<'a>() -> Result<ParsedAllowOrigin<'a>> {
+fn load_allowed_origins<'a>() -> Result<ParsedAllowOrigin> {
     let path = "data/cors_setting.json";
     if !PathBuf::from(path).exists() {
         File::create(path)?;
         let write = File::options().write(true).open(path)?;
         serde_json::to_writer(write, &ParsedAllowOrigin::default())?;
 
-        return Ok(ParsedAllowOrigin::default())
+        return Ok(ParsedAllowOrigin::default());
     }
 
     // scoped deserialize
@@ -48,7 +35,10 @@ fn load_allowed_origins<'a>() -> Result<ParsedAllowOrigin<'a>> {
 pub fn middleware_factory() -> Cors {
     Cors::default()
         .allowed_origin_fn(|origin_value, _| {
-            CORS_ALLOWED_DOMAINS.0.contains(&parse_domain_name(origin_value.to_str().unwrap()).unwrap())
+            CORS_ALLOWED_DOMAINS
+                .iter()
+                .find(|s| s.as_str() == origin_value.to_str().unwrap())
+                .is_some()
         })
         .allowed_headers([CONTENT_TYPE, AUTHORIZATION, ACCEPT, ACCEPT_LANGUAGE])
         .allowed_methods(["GET", "HEAD"])
